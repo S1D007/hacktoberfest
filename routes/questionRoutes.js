0df5adcd375f3573fbc45bbaecae2c372db99ba4
Siddhant <94226898+S1D007@users.noreply.github.com>
2022-10-16 21:05:42 +0530

    Update questionRoutes.js

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Question = require("../models/questionModel");
 const { questionSchema } = require("../validators/questionValidators");
 const validate = require("../middlewares/validateMiddleware");
+const cors = require("cors")
 const {
   addQuestion,
   getQuestions,
@@ -9,16 +10,16 @@ const {
   getQuestionsWithParam,
   practice,
   setCoinHistory,
+  getCoinHistory
 } = require("../controllers/questionController");
-
 const route = express.Router();
-
+route.use(cors())
 route.post("/add-questions", validate(questionSchema), addQuestion);
 route.get("/get-questions", getQuestions);
 route.get("/get-single-question", getSingleQuestion);
 route.get("/practice",practice)
 route.get("/coin-history",setCoinHistory)
-// route.get("/coin-history",getCoinHistory)
+route.get("/coin-history-get",getCoinHistory)
 const getRandomQuestions = async () => {
   const url = "https://opentdb.com/api.php?amount=100&type=multiple";
   const data = await axios.get(url);
