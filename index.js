85a785608e960a498c4e29572aa27e9acb9b39fd
Siddhant <94226898+S1D007@users.noreply.github.com>
2022-10-16 21:06:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,19 @@ connectToDatabase();
 const app = express();
 
 app.use(cors());
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:8100'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  return next();
+});
+// app.use(cors({
+//   origin: 'http://localhost:8100'
+// }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(questionRoutes);
 
 /**
